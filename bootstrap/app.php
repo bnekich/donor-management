070a23276b2fef9ba3f2b04deeb28925f78e70f5
bnekich <bnekich@gmail.com>
2026-01-24 15:40:56 -0600

    fixing trusthost error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies (needed for Digital Ocean App Platform and similar hosting)
         $middleware->trustProxies(at: '*');
-        $middleware->trustHosts(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
